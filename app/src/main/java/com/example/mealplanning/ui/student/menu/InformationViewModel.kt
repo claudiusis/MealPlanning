3534912c95fb6078d982c4845990f44b79d3bf38
Denis <plax1denis@mail.ru>
2024-03-03 20:51:11 +0300

    Fragment Information

---
+++ app/src/main/java/com/example/mealplanning/ui/student/menu/InformationViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.mealplanning.ui.student.menu
+
+import androidx.lifecycle.ViewModel
+
+class InformationViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/com/example/mealplanning/ui/student/menu/MenuFragment.kt
+++ app/src/main/java/com/example/mealplanning/ui/student/menu/MenuFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.mealplanning.R
 import com.example.mealplanning.databinding.FragmentMenuStudentBinding
 
 class MenuFragment : Fragment() {
@@ -31,6 +34,14 @@ class MenuFragment : Fragment() {
         notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         } */
+
+        binding.imageFood1.setOnClickListener {
+            val bundle = bundleOf()
+            bundle.putString("nameDish", binding.foodName1.text.toString())
+            findNavController().navigate(R.id.action_navigation_menu_student_to_informationFragment, bundle)
+        }
+
+
         return _binding!!.root
     }
 
