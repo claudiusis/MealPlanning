6ea6c151805f7463509292cc808c48447211bc8b
Artyom <artyomnalbandian@mail.ru>
2024-03-25 15:48:17 +0300

    Init commit

--- app/src/main/java/com/example/mealplanning/ui/student/main/MainFragment.kt
+++ app/src/main/java/com/example/mealplanning/ui/student/main/MainFragment.kt
@@ -14,29 +14,30 @@ import com.google.android.material.navigation.NavigationBarView
 class MainFragment : Fragment() {
 
 
-
-    private var _binding:FragmentMainBinding?=null
-    private val mBinding get()=_binding!!
+    private var _binding: FragmentMainBinding? = null
+    private val mBinding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding=FragmentMainBinding.inflate(inflater, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
         return mBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val navController=(childFragmentManager.findFragmentById(R.id.main_nav_container)
+        val navController = (childFragmentManager.findFragmentById(R.id.main_nav_container)
                 as NavHostFragment).navController
-        NavigationUI.setupWithNavController(view.findViewById(R.id.bottom_navigation_view)
-                as NavigationBarView, navController)
+        NavigationUI.setupWithNavController(
+            view.findViewById(R.id.bottom_navigation_view)
+                    as NavigationBarView, navController
+        )
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding=null
+        _binding = null
     }
 
 
