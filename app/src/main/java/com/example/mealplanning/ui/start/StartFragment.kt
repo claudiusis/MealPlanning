7490f7a114d245b41dc2a96b4874486f34d16589
Nikita <velikiy_2004@bk.ru>
2024-03-29 16:57:33 +0300

    Refactor gradle file

--- app/src/main/java/com/example/mealplanning/ui/start/StartFragment.kt
+++ app/src/main/java/com/example/mealplanning/ui/start/StartFragment.kt
@@ -26,7 +26,9 @@ class StartFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this)[StartViewModel::class.java]
+        binding.studentBtn.setOnClickListener {
 
+        }
     }
 
 }
